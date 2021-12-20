56510a32430402b7e8fb74e55337f041cae5e4b7
Jhaser_journey <Jhaser0284@users.noreply.github.com>
2021-12-20 12:56:41 +0800

    update to manager view

--- java_ca/src/main/java/edu/nus/java_ca/JavaCaApplication.java
+++ java_ca/src/main/java/edu/nus/java_ca/JavaCaApplication.java
@@ -1,6 +1,5 @@
 package edu.nus.java_ca;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,10 +11,10 @@ public class JavaCaApplication {
 		SpringApplication.run(JavaCaApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-
-		};
-	}
+//	@Bean
+//	CommandLineRunner runner() {
+//		return args -> {
+//
+//		};
+//	}
 }
