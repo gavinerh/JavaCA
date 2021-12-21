8e0f9ec1d7f2fca4cddddb355521f4d07b90ebe9
Filzah <filzah@u.nus.edu>
2021-12-21 12:20:13 +0800

    Merge branch 'main' of https://github.com/gavinerh/JavaCA.git into main

--- java_ca/src/main/java/edu/nus/java_ca/repository/LeaveBalanceRepo.java
+++ java_ca/src/main/java/edu/nus/java_ca/repository/LeaveBalanceRepo.java
@@ -1,9 +1,14 @@
 package edu.nus.java_ca.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.nus.java_ca.model.LeaveBalance;
 
 public interface LeaveBalanceRepo extends JpaRepository<LeaveBalance, Integer> {
+	List<LeaveBalance> findByBalId(Long balId);
+	
+	List<LeaveBalance> findAll();
 
 }
