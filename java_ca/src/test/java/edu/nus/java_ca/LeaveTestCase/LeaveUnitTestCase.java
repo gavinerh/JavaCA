1b9a48229595cf30514b7aee2ba8d2b505bf6244
Joanne <Joanne@users.noreply.github.com>
2021-12-24 09:12:22 +0800

    clean up leaveunittestcase

--- java_ca/src/test/java/edu/nus/java_ca/LeaveTestCase/LeaveUnitTestCase.java
+++ java_ca/src/test/java/edu/nus/java_ca/LeaveTestCase/LeaveUnitTestCase.java
@@ -1,44 +1,44 @@
-//package edu.nus.java_ca.LeaveTestCase;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import java.time.LocalDate;
-//import java.util.ArrayList;
-//
-//import org.junit.jupiter.api.TestMethodOrder;
-//import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-//import org.junit.jupiter.api.Order;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import sg.iss.laps.JavaCaApplication;
-//import sg.iss.laps.model.Leave;
-//import sg.iss.laps.model.LeaveBalance;
-//import sg.iss.laps.model.LeaveStatus;
-//import sg.iss.laps.model.LeaveType;
-//import sg.iss.laps.model.User;
-//import sg.iss.laps.repository.LeaveBalanceRepo;
-//import sg.iss.laps.repository.LeaveRepo;
-//import sg.iss.laps.repository.UserRepo;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest(classes = JavaCaApplication.class)
-//@TestMethodOrder(OrderAnnotation.class)
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//public class LeaveUnitTestCase {
-//
-//	@Autowired
-//	LeaveRepo lrepo;
-//	@Autowired
-//	UserRepo urepo;
-//	@Autowired
-//	LeaveBalanceRepo lbrepo;
-//
-//	
+package edu.nus.java_ca.LeaveTestCase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import edu.nus.java_ca.JavaCaApplication;
+import edu.nus.java_ca.model.Department;
+import edu.nus.java_ca.model.Leave;
+import edu.nus.java_ca.model.LeaveBalance;
+import edu.nus.java_ca.model.LeaveStatus;
+import edu.nus.java_ca.model.User;
+import edu.nus.java_ca.repository.LeaveBalanceRepo;
+import edu.nus.java_ca.repository.LeaveRepo;
+import edu.nus.java_ca.repository.UserRepository;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = JavaCaApplication.class)
+@TestMethodOrder(OrderAnnotation.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class LeaveUnitTestCase {
+
+	@Autowired
+	LeaveRepo lrepo;
+	@Autowired
+	UserRepository urepo;
+	@Autowired
+	LeaveBalanceRepo lbrepo;
+
+	
 //	@Test
 //	@Order(1)
 //	public void testfindAllLeaveToApprove() {
@@ -49,47 +49,44 @@
 //		LocalDate d3 = LocalDate.parse("2022-02-04");
 //
 //		//user
-//		User u = new User((long)340, "Nike@com", "Nike", "Greek", d, "Branding", "12345678");
-//		User u1 = new User((long)341, "Zeus@com", "Zeus", "Greek", d, "Security", "12345678");
-//		User u2 = new User((long)342, "Hades@com", "Hades", "Greek", d, "Marketing", "12345678");
-//		User u3 = new User((long)343, "Persephone@com", "Persephone", "Greek", d, "Management", "12345678");
-//	
-//		User u4 = new User((long)340, "Nike");
-//		User u5 = new User((long)341, "Zeus");
-//		User u6 = new User((long)342, "Hades");
-//		User u7 = new User((long)343, "Persephone");
+//		User u = new User();User u1 = new User();User u2 = new User();User u3 = new User();
+//		u.setEmail("Zeus@gmail");u.setFirstName("Zeus"); u.setLastName("Greek");u.setPassword("password"); u.setDepartment(Department.HR);
+//		u1.setEmail("Nike@gmail");u1.setFirstName("Nike");u1.setLastName("Greek");u1.setPassword("password"); u1.setDepartment(Department.Coporate);
+//		u2.setEmail("Hade@gmail");u2.setFirstName("Hade"); u2.setLastName("Greek");u2.setPassword("password"); u2.setDepartment(Department.Financial);
+//		u3.setEmail("Pers@gmail");u3.setFirstName("Persephone"); u3.setLastName("Greek");u2.setPassword("password"); u2.setDepartment(Department.Production);
+//
 //		ArrayList<User> ulist = new ArrayList<User>();
 //		ulist.add(u);ulist.add(u1);ulist.add(u2);ulist.add(u3);
 //		urepo.saveAllAndFlush(ulist);
 //			
 //		LeaveStatus ap = LeaveStatus.APPLIED;
-//		LeaveType ml = LeaveType.MEDICALLEAVE; LeaveType cl = LeaveType.COMPENSATIONLEAVE;
-//		LeaveType al = LeaveType.ANNUALlEAVE;
 //		
-//		LeaveBalance lb = new LeaveBalance(123, u, al, 50);
-//		LeaveBalance lb1 = new LeaveBalance(124, u1, al, 50);
-//		LeaveBalance lb2 = new LeaveBalance(125, u2, al, 50);
-//		LeaveBalance lb3 = new LeaveBalance(126, u3, al, 50);
+//		//LEAVEBALANCE
+//		LeaveBalance lb = new LeaveBalance(123, "MEDICALLEAVE", 3, u);
+//		LeaveBalance lb1 = new LeaveBalance(124, "ANNUALLEAVE", 2, u1);
+//		LeaveBalance lb2 = new LeaveBalance(125, "ANNUALLEAVE", 4, u2);
+//		LeaveBalance lb3 = new LeaveBalance(126, "ANNUALLEAVE", 3, u3);
 //		ArrayList<LeaveBalance> lblist = new ArrayList<LeaveBalance>();
 //		lblist.add(lb);lblist.add(lb1);lblist.add(lb2);lblist.add(lb3);
 //		lbrepo.saveAllAndFlush(lblist);
-//		
-//		Leave l = new Leave(u, ap, ml, "cough, fever", 
+//			
+//		//LEAVE
+//		Leave l = new Leave(u, ap, "Medical Leave" , "cough, fever", 
 //				"George will take over the project temporarily", d, d, d2);
-//		Leave l1 = new Leave(u1, ap, cl, "Family time", 
+//		Leave l1 = new Leave(u1, ap, "Compensation leave", "Family time", 
 //				"George will cover my appointments.", d, d1, d3);
-//		Leave l2 = new Leave(u2, ap, al, "Anniversary", 
+//		Leave l2 = new Leave(u2, ap, "Annual Leave", "Anniversary", 
 //				"George will helm Project Delta for 1 day.", d1, d2, d3);
-//		Leave l3 = new Leave(u3, ap, al, "Anniversary", 
+//		Leave l3 = new Leave(u3, ap, "Annual Leave", "Anniversary", 
 //				"All appointments have been postponed", d1, d2, d3);
 //		ArrayList<Leave> list = new ArrayList<Leave>();
 //		list.add(l);list.add(l1);list.add(l2);list.add(l3);
 //		lrepo.saveAllAndFlush(list);
 //		
-//	ArrayList<Leave> llist = (ArrayList) lrepo.findLeaveToApprove(ap, LeaveStatus.UPDATED);
-//	for (Leave current : llist) {
-//		System.out.println(current.toString());
+//		ArrayList<Leave> llist = (ArrayList) lrepo.findLeaveToApprove(ap, LeaveStatus.UPDATED);
+//		for (Leave current : llist) {
+//			System.out.println(current.toString());
+//		}
+//		assertEquals(llist.size(), 4);
 //	}
-//	assertEquals(llist.size(), 4);
-//	}
-//}
+}
