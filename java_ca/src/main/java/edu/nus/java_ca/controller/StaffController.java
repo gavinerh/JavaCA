9ab08d2918f0a0aa96c7564769db8e90f6f0a05e
Ivan9660644 <R948118221@gmail.com>
2021-12-24 20:21:31 +0800

    clear

--- java_ca/src/main/java/edu/nus/java_ca/controller/StaffController.java
+++ java_ca/src/main/java/edu/nus/java_ca/controller/StaffController.java
@@ -127,12 +127,6 @@ public class StaffController {
 			return("staff/staff-new-leave");
 		}
 		
-		//check for approving officer to send email to
-		if(u.getApprovingOfficer() == null) {
-			model.addAttribute("errormsg", "**You have no approving officer**");
-			return("staff/staff-new-leave");
-		}
-		
 		/**Count the number of leaves and return error if the user has not enough leave**/
 		Long count = lservice.countLeaves(leave.getStartDate(), leave.getEndDate(),u);
 		System.out.println("Total leave days: "+count);
@@ -316,7 +310,7 @@ public class StaffController {
 		@GetMapping(value = "/leave/list/{id}")
 		public String list(@PathVariable("id") int id ,Model model, HttpSession session) {
 			
-			System.out.println("Page Size:" + id);
+			
 			this.pagesize= id;
 			User u = user(session);
 			int currentpage = 0;
