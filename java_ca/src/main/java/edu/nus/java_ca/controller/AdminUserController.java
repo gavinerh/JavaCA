c173b19bdae9b275c6411381528458c96623b8b9
Filzah <filzah@u.nus.edu>
2021-12-24 18:42:45 +0800

    no duplicate email

--- java_ca/src/main/java/edu/nus/java_ca/controller/AdminUserController.java
+++ java_ca/src/main/java/edu/nus/java_ca/controller/AdminUserController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -81,11 +82,12 @@ public class AdminUserController {
 		if (bindingResult.hasErrors()) {
 			return "admin/user-form";
 		}
-		if (Uservice.checkDuplicatesEmail(user.getEmail()))
-		{
-			model.addAttribute("errormsg", "**Email is used**");
+		User result = Uservice.findByUserEmail(user.getEmail());
+		if (result != null) { 
+			ObjectError err = new ObjectError("Username error", "Email is already used");
+			bindingResult.addError(err);
 			return "admin/user-form";
-		}
+			}
 		Collection<LeaveBalance> lb = new ArrayList<LeaveBalance>();
 
 		LeaveBalance lbAnnual = new LeaveBalance("annual", 18, user);
